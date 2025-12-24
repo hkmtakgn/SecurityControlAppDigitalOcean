14549fdee253669ce703332209d991dc60c906f8
hkmtakgn <hikmetakgun94@gmail.com>
2025-12-24 11:14:12 +0300

    'create-medifiles-settings-for-digital-ocean'

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join (BASE_DIR,"staticfiles")
-STATICFILES_DIRS = os.path.join (BASE_DIR,"local_static_files")
+STATICFILES_DIRS = (os.path.join (BASE_DIR,"local_static_files"),)
 
 
 # Default primary key field type
